78592b565f94206e795188d623c62fc3dda6065f
AyeSpacey <ayevenomics@gmail.com>
2021-09-06 21:44:48 +0530

    Added Playlist downloader script

---
+++ scripts/frozen.sh
@@ -0,0 +1,5 @@
+echo 'Updating local ^Frozen Playlist 🎵'
+cd $HDIR/Frozen
+pipx run spotdl https://open.spotify.com/playlist/1jIvfFh5Qv0JDc7NDqigVb
+cd $HDIR
+echo 'Updated local ^Frozen Playlist 🎵'
