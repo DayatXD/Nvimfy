6d8c45a14835313ad05fda9e2f9eda16b1f26cf5
Shantnu <shantnuprofid@gmail.com>
2021-10-28 16:36:24 +0530

    added install script

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/env bash
+
+CONF=$HOME/.config/nvim
+
+if [[ -d $CONF ]]
+then
+    OLD_CONF=$HOME/.config/nvim.$(date +'%Y-%m-%d_%R:%S')
+    mv -f $CONF $OLD_CONF
+    echo "moved moved your config to $OLD_CONF"
+fi
+
+git clone https://github.com/AyeSpacey/Nvimfy $HOME/.config/nvim
