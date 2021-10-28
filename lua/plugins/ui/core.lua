aec3da29dda6edd07fa54d7bfd42a0088c4e570f
ayesumit <88986285+AyeSpacey@users.noreply.github.com>
2021-10-28 22:57:51 +0530

    Update core.lua

--- lua/plugins/ui/core.lua
+++ lua/plugins/ui/core.lua
@@ -1,5 +1,5 @@
-require('plugins.ui.startify.lua')
-require('plugins.ui.menu.lua')
+require('plugins.ui.startify')
+require('plugins.ui.menu')
 
 local cmd = vim.cmd
 local g = vim.g
@@ -7,6 +7,6 @@ local g = vim.g
 g.move_key_modifier = 'C'
 
 g.indent_blankline_enabled = false
-g.indent_blankline_filetype_exclude = ['help', 'startify', 'dashboard']
+g.indent_blankline_filetype_exclude = { 'help', 'startify', 'dashboard' }
 
 g.cursorline_timeout = 2000
