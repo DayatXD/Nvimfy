fd3a57993908fcc9dd233eee31841050fa8811e9
ayespacey <ayevenomics@gmail.com>
2021-10-28 23:44:50 +0530

    convert startify to lua 🥲

---
+++ lua/plugins/ui/startify.lua
@@ -0,0 +1,48 @@
+local g = vim.g
+local cmd = vim.cmd                           local fn = vim.fn
+
+local nvimfy_header = {
+   '',
+   '   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆         ',
+   '    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ',        '          ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷⠄⠄⠄⠄⠻⠿⢿⣿⣧⣄     ',
+   '           ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀⠄⠢⣀⡀⠈⠙⠿⠄    ',
+   '          ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀   ',
+   '   ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘⠄ ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿⠄  ',
+   '  ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾⠄   ',
+   ' ⣠⣿⠿⠛⠄⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄  ',
+   ' ⠙⠃   ⣼⣿⡟  ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿ ⣿⣿⡇⠄⠛⠻⢷⣄ ',
+   '      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ',
+   '       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ',
+   '',
+ }
+local nvimfy_footer = {
+   '',
+   ' [ Happy Coding! ] ',
+ }
+
+g.startify_custom_header = fn['startify#center'](nvimfy_header)
+g.startify_custom_footer = fn['startify#center'](nvimfy_footer)
+
+g.startify_lists = {
+         { type = 'bookmarks'},
+         { type = 'commands'}
+         }
+g.startify_bookmarks = {
+         { b = '~/.bashrc' },
+         { i = '~/.config/nvim/settings/config.vim' }
+         }
+g.startify_commands = {
+         { f = {'Files', ':Telescope find_files'}},
+         { c = {'Colors', ':Telescope colorscheme'}}
+         }
+
+g.startify_enable_special = 0
+g.startify_padding_left = 2
+
+
+vim.cmd[[
+  augroup startify
+      autocmd!
+      autocmd User Startified setlocal cursorline
+  augroup END
+]]
