8b9286b1cdce1335596444e28178ba92ba6c0d55
Shantnu <shantnuprofid@gmail.com>
2021-10-28 17:06:05 +0530

    fixed typo

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ if [[ -d $CONF ]]
 then
     OLD_CONF=$HOME/.config/nvim.$(date +'%Y-%m-%d_%R:%S')
     mv -f $CONF $OLD_CONF
-    echo "moved moved your config to $OLD_CONF"
+    echo "moved your config to $OLD_CONF"
 fi
 
 git clone https://github.com/AyeSpacey/Nvimfy $HOME/.config/nvim
